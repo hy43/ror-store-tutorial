13c743081dcf0add5b689aca64a89becbd6a5687
hy43 <hyde0401@gmail.com>
2025-02-11 13:38:31 +0900

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "products#index"
+  
   get "/products", to: "products#index"
   get "/products/new", to: "products#new"
   post "/products/create", to: "products#create"
