9c1ba735820be1d0170b8bf3a5e4554e2cc13c08
hy43 <hyde0401@gmail.com>
2025-02-11 14:09:10 +0900

    modify routes individual path to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root "products#index"
-  
-  get "/products", to: "products#index"
-  get "/products/new", to: "products#new"
-  post "/products/create", to: "products#create"
-  get "/products/:id", to: "products#show"
-  patch "/products/:id", to: "products#edit"
-  put "/products/:id",  to: "products#update"
-  delete "/products/:id", to: "products#delete"
-  # resources :products
+
+  # get "/products", to: "products#index"
+  # get "/products/new", to: "products#new"
+  # post "/products/create", to: "products#create"
+  # get "/products/:id", to: "products#show"
+  # patch "/products/:id", to: "products#edit"
+  # put "/products/:id",  to: "products#update"
+  # delete "/products/:id", to: "products#delete"
+  resources :products
 end
